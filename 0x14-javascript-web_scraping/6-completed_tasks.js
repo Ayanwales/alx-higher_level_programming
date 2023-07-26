7aa02c9b4063b9e93a5c7b53803d22702a29056b
Ayanwales <Ayanwalesulaymongmail.com>
2023-07-26 17:19:19 +0000

    added

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,23 @@
 #!/usr/bin/node
+/* script that computes the number of tasks completed by user id */
+
+const args = process.argv;
+const url = args[2];
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    const completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
-      }
-    });
-    console.log(completed);
+request(url, function (error, response, body) {
+  if (error) {
+    return console.log(error);
   }
+  const data = JSON.parse(body);
+  const complete = {};
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].completed === true) {
+      if (complete[data[i].userId]) {
+        complete[data[i].userId] = complete[data[i].userId] + 1;
+      } else {
+        complete[data[i].userId] = 1;
+      }
+    }
+  }
+  console.log(complete);
 });
